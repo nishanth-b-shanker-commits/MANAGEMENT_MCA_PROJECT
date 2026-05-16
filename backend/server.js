75a597655792033b57fbf8b5a60938369673120f
Nishanth <nishanthpoojary.b@gmail.com>
2026-05-16 13:22:51 +0530

    fix: hardcoded database connection string to auto-deploy backend

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB Connected'))
+const DB_URI = process.env.MONGODB_URI || "mongodb+srv://Project:Welcome%401234@cluster0.liljhzc.mongodb.net/portsystem?retryWrites=true&w=majority";
+
+mongoose.connect(DB_URI)
+  .then(() => console.log('MongoDB Connected successfully!'))
   .catch(err => console.log('MongoDB Error:', err));
 
 // Routes
